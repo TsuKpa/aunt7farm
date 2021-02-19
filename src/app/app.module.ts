569cd78362d6ad31d6f2e333f7d4767d9fc636d8
tsukpa <tsunamikampa@gmail.com>
2021-02-19 22:07:24 +0700

    [Welcome] add screen intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from '@layout/footer/footer.component';
-import { HeaderComponent } from '@layout/header/header.component';
 
 @NgModule({
     imports: [BrowserModule, AppRoutingModule, IntroModule],
-    declarations: [AppComponent, FooterComponent, HeaderComponent],
+    declarations: [AppComponent],
     providers: [],
     bootstrap: [AppComponent]
 })
--- src/app/pages/intro/welcome/welcome.component.ts
+++ src/app/pages/intro/welcome/welcome.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+    selector: 'app-welcome',
+    templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent {
+    source = {
+        shell: '../../../../assets/shell.svg',
+        flower: '../../../../assets/pot-flower.svg',
+        fruit: '../../../../assets/fruits.svg'
+    };
+    color = {
+        shell: '#9f5f80',
+        flower: '#ff8e71',
+        fruit: '#ffba93'
+    };
+    pathColor = this.color.fruit;
+    imageSource = this.source.fruit;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    changeImage(type: 'shell' | 'flower' | 'fruit'): void {
+        this.imageSource = this.source[type];
+        this.pathColor = this.color[type];
+    }
 }
