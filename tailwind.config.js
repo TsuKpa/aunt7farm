fbb30dd12cbd324b066ad9e6d438cbf904da79ec
tsukpa <tsunamikampa@gmail.com>
2021-02-28 21:36:52 +0700

    add config build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-  prefix: '',
-  purge: {
-    content: [
-      './src/**/*.{html,ts}',
-    ]
-  },
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['active', 'checked'],
-      opacity: ['disabled'],
-      borderColor: ['active', 'checked'],
-    }
-  },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography')
-  ],
+	prefix: '',
+	purge: {
+		enabled: true,
+		content: [
+			'./src/**/*.{html,ts}',
+		]
+	},
+	darkMode: 'class', // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {
+			backgroundColor: ['active', 'checked'],
+			opacity: ['disabled'],
+			borderColor: ['active', 'checked'],
+		}
+	},
+	plugins: [
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/typography')
+	],
 };
